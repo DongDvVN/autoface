a2717fbf57027712d78c8ca537de59a3e7e41a09
Đông Đoàn <dongdv@tima.vn>
2022-12-13 15:20:13 +0700

    push code

--- Winform.AutoSendSms/Winform.AutoSendSms/Program.cs
+++ Winform.AutoSendSms/Winform.AutoSendSms/Program.cs
@@ -16,7 +16,7 @@ namespace Winform.AutoSendSms
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new AutoSendSms());
         }
     }
 }
