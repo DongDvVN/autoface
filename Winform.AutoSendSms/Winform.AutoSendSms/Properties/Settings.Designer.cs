88bbc7d63dd44c71d5541241dd252dd0e81bed96
Đông Đoàn <dongdv@tima.vn>
2022-12-16 16:39:21 +0700

    push code

--- Winform.AutoSendSms/Winform.AutoSendSms/Properties/Settings.Designer.cs
+++ Winform.AutoSendSms/Winform.AutoSendSms/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Biglead.Autoinbox.Properties {
+namespace Winform.AutoSendSms.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
